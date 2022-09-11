5091ede42b631e67ce72c8102da8a55b36cf1be9
bilguun111101 <Mason@Masons-MacBook-Pro.local>
2022-09-12 07:23:44 +0800

    Movies date and quantity of people

---
+++ src/components/Film/FilmStyle.js
@@ -0,0 +1,50 @@
+export const styles = {
+    filmProduct: {
+        display: "flex",
+        justifyContent: "center",
+        width: "100vw",
+        marginTop: "2em",
+        paddingBottom: "4em"
+    },
+    filmGrid: {
+        justifyContent: "center"
+    },
+    gridItem: {
+        width: "300px",
+        height: "auto"
+    },
+    filmTicketsImage: {
+        width: "100%"
+    },
+    dataGrid: {
+        justifyContent: "center",
+        gap: "1rem"
+    },
+    dateBox: {
+        color: "red",
+        border: "1px solid #FFF",
+        textAlign: "center",
+        width: "120px",
+        color: "#FFF",
+        boxShadow: "0 0 5px grey"
+    },
+    continue: {
+        color: "red",
+        fontSize: "1.4em"
+    },
+    input: {
+        input: {color: "red"},
+        height: "3em",
+        marginBottom: "1em",
+    },
+    inputTicket: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputOfBox: {
+        width: "100%",
+        marginBottom: "1em"
+    }
+}
