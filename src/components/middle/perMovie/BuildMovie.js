0e4295264015becaf1009d288ec2f1655fc86afc
bilguun70b1123@gmail.com <bilguun70b1123@gmail.com>
2022-09-12 08:43:15 +0800

    Build piece of movies

--- src/components/middle/perMovie/BuildMovie.js
+++ src/components/middle/perMovie/BuildMovie.js
@@ -1,37 +1,56 @@
 export const styles = {
-    myButton: {
-      "&:hover": {
-        backgroundColor: "red",
-        color: "#FFF",
-      },
-      backgroundColor: "orange",
-      color: "black",
-      zIndex: "-1",
+  myButton: {
+    "&:hover": {
+      backgroundColor: "red",
+      color: "#FFF",
     },
-    orangeBtn: {
-      backgroundColor: "#ff9800",
+    backgroundColor: "orange",
+    color: "black",
+    zIndex: "-1",
+  },
+  orangeBtn: {
+    backgroundColor: "#ff9800",
+    width: "60%",
+    color: "#FFF",
+    fontSize: "80%",
+    "&:hover": {
+      opacity: "0.9",
+    },
+  },
+  gridItem: (theme) => ({
+    width: "20%",
+    paddingLeft: "1rem",
+    backgroundColor: "#000",
+    [theme.breakpoints.down("lg")]: {
+      width: "30%",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "40%",
+    },
+    [theme.breakpoints.down("sm")]: {
       width: "50%",
-      fontSize: "1rem",
-      "&:hover": {
-        opacity: "0.9",
-      },
     },
-    gridItem: (theme) => ({
-      width: "20%",
-      paddingLeft: "1rem",
-      backgroundColor: "#000",
-      [theme.breakpoints.down("lg")]: {
-        width: "30%",
-      },
-      [theme.breakpoints.down("md")]: {
-        width: "40%",
-      },
-      [theme.breakpoints.down("sm")]: {
-        width: "80%",
-      },
-    }),
-    name: {
-      fontSize: "1.5rem",
-      color: "#FFF"
-    }
-  };
+  }),
+  name: (theme) => ({
+    fontSize: "1.5rem",
+    color: "#FFF",
+    marginBottom: "1em",
+    textShadow: "0 0 5px #FFF",
+    [theme.breakpoints.down("lg")]: {
+      fontSize: "150%",
+    },
+  }),
+  cartSection: (theme) => ({
+    padding: "1.5rem",
+    width: "60%",
+    [theme.breakpoints.down("lg")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "50%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "30%",
+    },
+  }),
+};
--- src/components/middle/perMovie/moviePer.jsx
+++ src/components/middle/perMovie/moviePer.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useArrayVarContext } from "../../../context";
 import { Button, Card, Box, Grid, Typography, CardMedia } from "@mui/material";
-import { styles } from "./BuildMovie"
+import { styles } from "./BuildMovie";
 
 const PerMovie = (props) => {
   const data = props.data;
@@ -19,21 +19,20 @@ const PerMovie = (props) => {
 
   return (
     <Grid item spacing={2} sx={styles.gridItem}>
-      <Card padding={1.5} sx={{ minWidth: 200 }}>
-        <Box
-          sx={{ backgroundColor: "primary.main" }}
-        >
-          <CardMedia src={data.image} alt="" component="img" />
+      <Card>
+        <Box sx={{ backgroundColor: "primary.main" }}>
+          <CardMedia
+            sx={{ width: "100%", height: "auto" }}
+            src={data.image}
+            alt=""
+            component="img"
+          />
           <Typography component="h2" sx={styles.name}>
             {data.name}
           </Typography>
           <Box display="flex" sx={{ justifyContent: "space-between" }}>
             <Typography />
-            <Button
-              sx={styles.orangeBtn}
-              variant="contained"
-              onClick={handleFilm}
-            >
+            <Button sx={styles.orangeBtn} onClick={handleFilm}>
               Захиалах
             </Button>
           </Box>
